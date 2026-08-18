76f9e8215ce18ce2db94389dc9fb92b3fb3d1082
Harshit Khurana <hkharshitkhurana@gmail.com>
2026-08-18 19:12:04 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -4,12 +4,15 @@ import { Server } from "socket.io"
 const app=express()
 const server=http.createServer(app)
 
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:5173",
-        methods:["GET","POST"]
+const io = new Server(server, {
+    cors: {
+        origin: [
+            "http://localhost:5173",
+            "https://vybe-sbl1.onrender.com"
+        ],
+        credentials: true
     }
-})
+});
 
 const userSocketMap={}
 
