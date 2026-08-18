043c557afbe66f405a859898691890afe62f704c
Harshit Khurana <hkharshitkhurana@gmail.com>
2026-08-18 22:28:58 +0530

    Update getCurrentUser.jsx

--- frontend/src/hooks/getCurrentUser.jsx
+++ frontend/src/hooks/getCurrentUser.jsx
@@ -1,25 +1,35 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setFollowing, setUserData } from '../redux/userSlice'
-import { setCurrentUserStory } from '../redux/storySlice'
+import axios from "axios";
+import { useEffect } from "react";
+import { serverUrl } from "../App";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../redux/userSlice";
+import { setCurrentUserStory } from "../redux/storySlice";
 
 function getCurrentUser() {
-    const dispatch=useDispatch()
-    const {storyData}=useSelector(state=>state.story)
-  useEffect(()=>{
-const fetchUser=async ()=>{
-    try {
-        const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
-         dispatch(setUserData(result.data))
-         dispatch(setCurrentUserStory(result.data.story))
-    } catch (error) {
-        console.log(error)
-    }
-}
-fetchUser()
-  },[storyData])
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const result = await axios.get(
+                    `${serverUrl}/api/user/current`,
+                    {
+                        withCredentials: true
+                    }
+                );
+
+                dispatch(setUserData(result.data));
+                dispatch(setCurrentUserStory(result.data.story));
+
+            } catch (error) {
+                console.log("Current user error:", error);
+
+                dispatch(setUserData(null));
+            }
+        };
+
+        fetchUser();
+    }, [dispatch]);
 }
 
-export default getCurrentUser
+export default getCurrentUser;
